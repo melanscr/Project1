f9d561e746b475d1e9fa52bad253a29a06b28c1d
melanscr <73835261+melanscr@users.noreply.github.com>
2021-01-04 16:17:17 -0500

    Code changes

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-let dateElement = document.querySelector("h1");
+let dateElement = document.querySelector("h2");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
